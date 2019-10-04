060c6f1cd96d7551fa06f291e8181939b5b20de1
childressdev <jcampbell@childressagency.com>
2019-10-04 17:54:51 -0400

    small carousel content

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -3,5 +3,29 @@
 */
 
 jQuery(document).ready(function($){
+  $('#featured-events-carousel').on('slide.bs.carousel', function(e){
+    var $slide_content_container = $('#featured-event');
+    var $new_slide = e.relatedTarget;
+    var event_date = '<h4 class="subtitle">' + $($new_slide).data('event_date') + '</h4>';
+    var event_desc = $($new_slide).data('event_desc');
+    var event_link = $($new_slide).data('event_link');
 
+    $slide_content_container.children().fadeOut('fast', function(){
+      $(this).remove();
+      $($slide_content_container).html(event_date + event_desc + event_link).fadeIn('fast');
+    });
+  });
+
+  $('#where-to-stay-carousel').on('slide.bs.carousel', function(e){
+    var $slide_content_container = $('#where-to-stay');
+    var $new_slide = e.relatedTarget;
+    var stay_location = '<h4 class="subtitle">' + $($new_slide).data('stay_location') + '</h4>';
+    var stay_desc = $($new_slide).data('stay_description');
+    var stay_link = $($new_slide).data('stay_link');
+
+    $slide_content_container.children().fadeOut('fast', function(){
+      $(this).remove();
+      $($slide_content_container).html(stay_location + stay_desc + stay_link).fadeIn('fast');
+    });
+  });
 });
