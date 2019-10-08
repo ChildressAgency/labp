169a0fb5e07cc28dc605d3c44bed89253984ec9f
childressdev <jcampbell@childressagency.com>
2019-10-08 17:57:10 -0400

    blog-news demo not styled

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -28,4 +28,52 @@ jQuery(document).ready(function($){
       $($slide_content_container).html(stay_location + stay_desc + stay_link).fadeIn('fast');
     });
   });
-});
+
+  $('#news-carousel.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if(!next.length){
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  });
+
+  $('#news-carousel.carousel-heights .carousel-inner .carousel-item').carouselHeights();
+
+}); //jQuery
+
+/**
+ * Normalize Carousel Heights
+ */
+$.fn.carouselHeights = function () {
+  var items = $(this), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest; //create variable to make note of the tallest slide
+
+  var normalizeHeights = function () {
+    items.each(function () { //add heights to array
+      heights.push($(this).height());
+    });
+    tallest = Math.max.apply(null, heights); //cache largest value
+    if (tallest < 300) { tallest = 300; }
+    items.each(function () {
+      $(this).css('min-height', tallest + 'px');
+    });
+  };
+
+  normalizeHeights();
+
+  $(window).on('resize orientationchange', function () {
+    //reset vars
+    tallest = 0;
+    heights.length = 0;
+
+    items.each(function () {
+      $(this).css('min-height', '0'); //reset min-height
+    });
+    normalizeHeights(); //run it again 
+  });
+
+  $('.locations-map').each(function () {
+    map = new_map($(this));
+  });
+};
